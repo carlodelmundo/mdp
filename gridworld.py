39bdcfadad2a0a66e400c35cd8350445b3c89715
Gagan Bansal <bansalg@cs.washington.edu>
2017-02-04 21:30:50 -0800

    Enable display for rtdp

--- gridworld.py
+++ gridworld.py
@@ -563,7 +563,7 @@ if __name__ == '__main__':
     ###########################
     # DISPLAY Q/V VALUES BEFORE SIMULATION OF EPISODES
     try:
-        if not opts.manual and opts.agent == 'value':
+        if not opts.manual and (opts.agent == 'value' or opts.agent == 'rtdp'):
             if opts.valueSteps:
                 for i in range(opts.iters):
                     tempAgent = valueIterationAgents.ValueIterationAgent(mdp, opts.discount, i)
